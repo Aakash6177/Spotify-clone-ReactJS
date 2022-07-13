54d6a1ab456e76695726304bf0544eaec7ac5ccc
Aakash6177 <www.aakash7469@gmail.com>
2022-07-13 16:38:37 -0700

    Made spotify.js error free

--- src/Login.js
+++ src/Login.js
@@ -5,7 +5,10 @@ import { loginUrl } from "./spotify";
 function Login() {
   return (
     <div className="login">
-      <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png" />
+      <img
+        src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_White.png"
+        alt=""
+      />
       <br />
       <a href={loginUrl}>LOG IN WITH SPOTIFY</a>
     </div>
--- src/spotify.js
+++ src/spotify.js
@@ -1,8 +1,9 @@
+// this is the latetest version of spotify.js file
 // using the spotify api for user authentication logic
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // after authentication it will redirect to the home page that is the local host 3000
-const reduirectUri = "https://localhost:3000";
+const redirectUri = "http://localhost:3000";
 
 // setting up some variables for later use
 const client_id = "a5fb2dc08e7c4d3491f52e128625ec1d";
@@ -17,6 +18,6 @@ const scopes = [
 ];
 
 // use all of the stuff above and make a login url using string interpilation
-export const loginUrl = `${authEndpoint}?client_id = ${client_id}&redirect_uri = ${reduirectUri}&scope = ${scopes.join(
+export const loginUrl = `${authEndpoint}?client_id=${client_id}&redirect_uri=${redirectUri}&scope=${scopes.join(
   "%20"
 )}&response_type=token&show_dialog=true`;
